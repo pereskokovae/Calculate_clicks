765340559e22ae6ea74921118ae8530a9ba6bb44
pereskokovae <pereskokovae48@gmail.com>
2025-03-21 00:19:11 +0700

    Исправила баг

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def shorten_link(url, token):
         "access_token": token,
         "url": url,
         'v': '5.199'
-        }
+    }
     response = requests.get(api_url, params=payload)
     response.raise_for_status()
     return response.json()['response']['short_url']
@@ -23,15 +23,14 @@ def count_clicks(short_link_part, token):
         "key": short_link_part,
         "v": "5.199",
         "interval": "forever"
-        }
+    }
     response = requests.get(api_url, params=payload)
     response.raise_for_status()
     return response.json()['response']['stats']
 
 
-def is_shorten_link(url, token):
+def is_shorten_link(short_link_part, token):
     api_url = 'https://api.vk.com/method/utils.getLinkStats'
-    short_link_part = os.path.basename(url.path)
     payload = {
         "access_token": token,
         "key": short_link_part,
@@ -41,25 +40,30 @@ def is_shorten_link(url, token):
     try:
         response = requests.get(api_url, params=payload)
         response.raise_for_status()
-        return True 
-    except requests.exceptions.HTTPError:
+        data = response.json()
+        
+        if 'error' in data:
+            return False
+        else:
+            return True
+    
+    except requests.RequestException:
         return False
 
 
 def main():
     load_dotenv()
-
+    
     token = os.environ['TG_TOKEN']
     long_url = input("Введите ссылку: ")
     url = urlparse(long_url)
 
     try:
-        if is_shorten_link(url, token):
-            short_link_part = os.path.basename(url.path)
-            clicks_count = count_clicks(short_link_part, token)
+        if is_shorten_link(long_url, token):
+            clicks_count = count_clicks(long_url, token)
             print('Количество кликов по сокращенной ссылке:', clicks_count)
         else:
-            short_link = shorten_link(url.geturl(), token)
+            short_link = shorten_link(long_url, token)
             print('Сокращенная ссылка:', short_link)
     except requests.exceptions.HTTPError as err:
         print(f"Ошибка при обращении к API, проверьте правильность ввода: {err}")
@@ -68,17 +72,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-
-
-
-
-
-
-
-
-
-
-
-        
 
